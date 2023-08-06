eb8d44a13a489df3e158a5afbb00de1cc76a8a20
Erick Hene <djefrisonferdinan@gmail.com>
2023-08-07 03:32:49 +0800

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Kategori from "./pages/kategori/Kategori"
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 function App() {
+
+  const handle = useFullScreenHandle();
+
   return (
     <>
-      <Kategori />
+      {/* <button onClick={handle.enter} className="btn-full">
+        Enter Fullscreen
+      </button> */}
+      {/* <FullScreen handle={handle}> */}
+        <Kategori />
+      {/* </FullScreen> */}
     </>
   )
 }
